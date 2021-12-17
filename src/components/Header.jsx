b48f1ed21662f8d3a066c083fb74d3582d6929cd
AntonOnoprienko <onoprienkoanton88@gmail.com>
2021-12-17 21:46:56 +0200

    .module.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
+import classes from './Header.module.css';
+
+
+
 const Header = () => {
 	return (
-		<header className='header'>
+		<header className={classes.header}>
 			  <img src='https://www.awwwards.com/awards/images/2015/08/yondr-awwwards-logos-4.gif' />
 		</header>
 	)
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,9 @@
+import classes from './Profile.module.css';
+
+
 const Profile = () => {
 	return (
-		<div className='content'>
+		<div className={classes.content}>
 			  <div>
 			  	<img className='walpapper' src='https://static-sl.insales.ru/images/products/1/328/193446216/11-019_%D0%9D%D1%8C%D1%8E-%D0%99%D0%BE%D1%80%D0%BA_%D1%87%D0%B1.jpg' />
 			  </div>
@@ -12,15 +15,15 @@ const Profile = () => {
 				  <p>25.01.1988</p>
 				  			<p>JavaScript Developer</p>
 			  </div>
-			  <div>
+			<div className={classes.posts}>
 				  My posts
-				  <div>
+				  <div >
 					  New post 
 				</div>
-				   <div>
+				<div className={classes.item}>
 					  Post1
  					</div>
-				  <div>
+				<div className={classes.item}>
 					  Post2
 				  </div>
 			  </div>
