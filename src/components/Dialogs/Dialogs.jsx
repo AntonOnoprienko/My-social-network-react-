ebb241864194b99c68780511f1fb6f8a45ab5eb4
AntonOnoprienko <onoprienkoanton88@gmail.com>
2021-12-22 21:37:57 +0200

    Update Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,32 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = (props) => {
 	return (
-		<div className= { classes.content} >
-			DIALOGS
+		<div className = { classes.dialogs } >
+			<div className = {classes.dialogsItems }>
+				<div className = { classes.dialog }>
+					Arthur Onoprienko
+				</div>
+				<div className = {classes.dialog} >
+					Vitalii Shmatko
+				</div>
+				<div className = { classes.dialog }>
+					Daria ♥ Onoprienko
+				</div>
+				<div className = { `${classes.dialog} + ${classes.active } `}>
+					Denis TitovGym
+				</div>
+				<div className = { classes.dialog }>
+					Swetlana Lubaya
+				</div>
+				<div className = { classes.dialog }>
+					Ksenya Gulchinskaya
+				</div>
+			</div>
+			<div className= {classes.messages }>
+				<div className= { classes.message }>Hi!</div>
+			<div className= { classes.message }>How is yours?</div>
+			<div className= { classes.message }>Fine!</div>
+			</div>
 		</div>
 	)
 }
--- src/components/Profile/My posts/MyPosts.jsx
+++ src/components/Profile/My posts/MyPosts.jsx
@@ -11,11 +11,11 @@ const MyPosts = () => {
 				<div >
 					   
 				</div>
-				<Post src='http://risovach.ru/thumb/upload/200s400/2014/07/generator/vau_55876037_orig_.jpeg?5u8zq' name='Артур Оноприенко 'message='Всем привет я тут новенький!!' likesCount='23'/>
-				<Post src='https://www.meme-arsenal.com/memes/a5dd2f55b36488a10172f4f84352846b.jpg' name='Виталик Шматько' message='Это будет собачья вписка' likesCount='0' />
-				<Post src='https://meragor.com/files/styles//ava_800_800_wm/standoff_1q.png' name='Денис TitovGym'message='Приходите качать банки!' likesCount='3' />
-				<Post src='https://www.meme-arsenal.com/memes/943b2cfb8850675319679ce91ebc5609.jpg' name='Dafna' message='it is very nice'likesCount='0' />
-				<Post src='https://shapka-youtube.ru/wp-content/uploads/2021/04/prikolnaya-avatarka.jpg' name='Mark 'message='I am happy' likesCount='0'/>
+				<Post src='http://risovach.ru/thumb/upload/200s400/2014/07/generator/vau_55876037_orig_.jpeg?5u8zq' name='Arthur Onoprienko'message='Всем привет я тут новенький!!' likesCount='23'/>
+				<Post src='https://www.meme-arsenal.com/memes/a5dd2f55b36488a10172f4f84352846b.jpg' name='Vitalii Shmatko' message='Это будет собачья вписка' likesCount='0' />
+				<Post src='https://meragor.com/files/styles//ava_800_800_wm/standoff_1q.png' name='Denis TitovGym'message='Приходите качать банки!' likesCount='3' />
+				<Post src='https://www.meme-arsenal.com/memes/943b2cfb8850675319679ce91ebc5609.jpg' name='Ksenia Gulchinskaya' message='it is very nice'likesCount='0' />
+				<Post src='https://shapka-youtube.ru/wp-content/uploads/2021/04/prikolnaya-avatarka.jpg' name='Mark Onoprienko'message='I am happy' likesCount='0'/>
 				
 			  </div>
 		  </div>
