cf154777f3cb372a25f1b114b6dfa13f4c67276c
AntonOnoprienko <onoprienkoanton88@gmail.com>
2021-12-24 20:55:41 +0200

    Array method .map

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -17,48 +17,39 @@ const Message = (props) => {
 
 const Dialogs = (props) => {
 	let dialogsData = [
-	{ id: 1, name: 'Arthur Onoprienko', url:'http://risovach.ru/thumb/upload/200s400/2014/07/generator/vau_55876037_orig_.jpeg?5u8zq' },
-	{ id: 2, name: 'Denis Titov', url:'https://meragor.com/files/styles//ava_800_800_wm/standoff_1q.png' },
-	{ id: 3, name: 'Daria Onoprienko', url:'https://i.pinimg.com/736x/84/dd/6e/84dd6e50b90c7bf1319ebbc3711cfff4.jpg' },
-	{ id: 4, name: 'Swetlana  Lubaya', url:'https://f1.upet.com/A_r2u6uZhnxA_x.jpg' },
-	{ id: 5, name: 'Ksenia Gulchinskaya', url:'https://author.today/content/2021/03/25/8488dde4a706465f96bf00ec457e4ec3.png' },
-	{ id: 6, name: 'Anton Taran', url:'https://download-cs.net/steam/avatars/3434.jpg' },
-	{ id: 7, name: 'Vitalii Shmatko', url:'https://www.meme-arsenal.com/memes/a5dd2f55b36488a10172f4f84352846b.jpg' }
+		{ id: 1, name: 'Denis Titov', url:'https://meragor.com/files/styles//ava_800_800_wm/standoff_1q.png' },
+		{ id: 2, name: 'Arthur Onoprienko', url:'http://risovach.ru/thumb/upload/200s400/2014/07/generator/vau_55876037_orig_.jpeg?5u8zq' },
+		{ id: 3, name: 'Daria Onoprienko', url:'https://i.pinimg.com/736x/84/dd/6e/84dd6e50b90c7bf1319ebbc3711cfff4.jpg' },
+		{ id: 4, name: 'Swetlana  Lubaya', url:'https://f1.upet.com/A_r2u6uZhnxA_x.jpg' },
+		{ id: 5, name: 'Ksenia Gulchinskaya', url:'https://author.today/content/2021/03/25/8488dde4a706465f96bf00ec457e4ec3.png' },
+		{ id: 6, name: 'Anton Taran', url:'https://download-cs.net/steam/avatars/3434.jpg' },
+		{ id: 7, name: 'Vitalii Shmatko', url:'https://www.meme-arsenal.com/memes/a5dd2f55b36488a10172f4f84352846b.jpg' }
+	];
+	
+	let messageData = [
+		{ id: 1, message: 'Hi!', url: 'http://risovach.ru/thumb/upload/200s400/2014/07/generator/vau_55876037_orig_.jpeg?5u8zq' },
+		{ id: 1, message: 'Hi!', url: 'http://risovach.ru/thumb/upload/200s400/2014/07/generator/vau_55876037_orig_.jpeg?5u8zq' },
+		{ id: 2, message: 'How is your life?', url: 'http://risovach.ru/thumb/upload/200s400/2014/07/generator/vau_55876037_orig_.jpeg?5u8zq' },
+		{ id: 3, message: 'Hi, i am fine', url: 'https://bipbap.ru/wp-content/uploads/2019/07/Samye-prikolnye-kartinki-dlya-devochek-dlya-srisovki-na-bumagu-1.jpg' },
+		{ id: 4, message: 'What are you doing?', url: 'http://risovach.ru/thumb/upload/200s400/2014/07/generator/vau_55876037_orig_.jpeg?5u8zq' },
+		{ id: 5, message: 'I want to go for a walk', url: 'https://bipbap.ru/wp-content/uploads/2019/07/Samye-prikolnye-kartinki-dlya-devochek-dlya-srisovki-na-bumagu-1.jpg' },
+		{ id: 6, message: 'Its good', url: 'http://risovach.ru/thumb/upload/200s400/2014/07/generator/vau_55876037_orig_.jpeg?5u8zq' }
+	
 	];
 
-	let messageData = [
-	{	id: 1, message: 'Hi!', url:'http://risovach.ru/thumb/upload/200s400/2014/07/generator/vau_55876037_orig_.jpeg?5u8zq'},
-	{  id: 2, message: 'How is your life?', url:'http://risovach.ru/thumb/upload/200s400/2014/07/generator/vau_55876037_orig_.jpeg?5u8zq'},
-	{  id: 3, message: 'Hi, i am fine', url: 'https://bipbap.ru/wp-content/uploads/2019/07/Samye-prikolnye-kartinki-dlya-devochek-dlya-srisovki-na-bumagu-1.jpg'},
-	{  id: 4, message: 'What are you doing?', url:'http://risovach.ru/thumb/upload/200s400/2014/07/generator/vau_55876037_orig_.jpeg?5u8zq'},
-	{  id: 5, message: 'I want to go for a walk', url: 'https://bipbap.ru/wp-content/uploads/2019/07/Samye-prikolnye-kartinki-dlya-devochek-dlya-srisovki-na-bumagu-1.jpg'},
-	{  id: 6, message: 'Its good', url:'http://risovach.ru/thumb/upload/200s400/2014/07/generator/vau_55876037_orig_.jpeg?5u8zq'}
-	
-	]
-		return (
+	let dialogsElements = dialogsData.map ( d => <DialogItem id= {d.id} name= {d.name} url= { d.url } />)
+	let messagesElements = messageData.map ( m => <Message message={  m.message} url={ m.url }/> )
+		
+	return (
 		<div className = { classes.dialogs } >
 			<div className = {classes.dialogsItems }>
-					<DialogItem id= {dialogsData[0].id} name= {dialogsData[0].name} url= { dialogsData[0].url } />
-					<DialogItem id= {dialogsData[1].id} name= {dialogsData[1].name} url= { dialogsData[1].url } />
-					<DialogItem id= {dialogsData[2].id} name= {dialogsData[2].name} url= { dialogsData[2].url } />
-					<DialogItem id= {dialogsData[3].id} name= {dialogsData[3].name} url= { dialogsData[3].url } />
-					<DialogItem id= {dialogsData[4].id} name= {dialogsData[4].name} url= { dialogsData[4].url } />
-					<DialogItem id= {dialogsData[5].id} name= {dialogsData[5].name} url= { dialogsData[5].url } />
-					<DialogItem id= {dialogsData[6].id} name= {dialogsData[6].name} url= { dialogsData[6].url } />
-				
+					{ dialogsElements }
 			</div>
 			
 
 			<div className= {classes.messages }>
-				<Message message={  messageData[0].message} url={ messageData[0].url }/>
-				<Message message={  messageData[1].message} url={ messageData[1].url }/>
-				<Message message={  messageData[2].message} url={ messageData[2].url }/>
-				<Message message={  messageData[3].message} url={ messageData[3].url }/>
-				<Message message={  messageData[4].message} url={ messageData[4].url }/>
-				<Message message={  messageData[5].message} url={ messageData[5].url }/>
-				
-				
-			</div>
+					{ messagesElements }
+				</div>
 		</div>
 	)
 }
--- src/components/Profile/My posts/MyPosts.jsx
+++ src/components/Profile/My posts/MyPosts.jsx
@@ -4,13 +4,17 @@ import Post from './Post/Post';
 
 const MyPosts = () => {
 	let postsData = [
-	{ id: 1, name: 'Arthur Onoprienko',message:'Всем привет я тут новенький!!', url:'https://www.blast.hk/attachments/68493/', likeCount: 5 },
-	{ id: 2, name: 'Denis Titov', message:'Я качаю банки как зверь!', url:'https://meragor.com/files/styles//ava_800_800_wm/standoff_1q.png', likeCount: 0 },
-	{ id: 3, name: 'Daria Onoprienko', message:'Будешь ехать домой, купи чипсиков!', url:'https://i.pinimg.com/736x/84/dd/6e/84dd6e50b90c7bf1319ebbc3711cfff4.jpg',likeCount: 3 },
-	{ id: 4, name: 'Ksenia Gulchinskaya', message:'Я опаздываю на работу', url:'https://author.today/content/2021/03/25/8488dde4a706465f96bf00ec457e4ec3.png',likeCount: 12 },
-	{ id: 5, name: 'Anton Taran', message:'Тебе еду сегодня привозить?', url:'https://download-cs.net/steam/avatars/3434.jpg',likeCount: 72 },
-	{ id: 6, name: 'Vitalii Shmatko', message:'Это будет собачья вписка', url:'https://www.meme-arsenal.com/memes/a5dd2f55b36488a10172f4f84352846b.jpg',likeCount: 23 }
-	]
+		{ id: 1, name: 'Arthur Onoprienko', message: 'Всем привет я тут новенький!!', url: 'https://www.blast.hk/attachments/68493/', likeCount: 5 },
+		{ id: 2, name: 'Denis Titov', message: 'Я качаю банки как зверь!', url: 'https://meragor.com/files/styles//ava_800_800_wm/standoff_1q.png', likeCount: 0 },
+		{ id: 3, name: 'Daria Onoprienko', message: 'Будешь ехать домой, купи чипсиков!', url: 'https://i.pinimg.com/736x/84/dd/6e/84dd6e50b90c7bf1319ebbc3711cfff4.jpg', likeCount: 83 },
+		{ id: 4, name: 'Ksenia Gulchinskaya', message: 'Я опаздываю на работу', url: 'https://author.today/content/2021/03/25/8488dde4a706465f96bf00ec457e4ec3.png', likeCount: 12 },
+		{ id: 5, name: 'Anton Taran', message: 'Тебе еду сегодня привозить?', url: 'https://download-cs.net/steam/avatars/3434.jpg', likeCount: 72 },
+		{ id: 6, name: 'Vitalii Shmatko', message: 'Это будет собачья вписка', url: 'https://www.meme-arsenal.com/memes/a5dd2f55b36488a10172f4f84352846b.jpg', likeCount: 23 },
+		{ id: 7, name: 'Vitalii Shmatko', message: 'Вы же прийдете?', url: 'https://www.meme-arsenal.com/memes/a5dd2f55b36488a10172f4f84352846b.jpg', likeCount: 2 },
+		{ id: 8, name: 'Denis Titov', message: 'Я точно буду!', url: 'https://meragor.com/files/styles//ava_800_800_wm/standoff_1q.png', likeCount: 0 },
+	];
+
+	let postsElements = postsData.map (p => <Post src={p.url } name={p.name } message={p.message } likesCount={p.likeCount } />)
 	return (
 		<div className={classes.content}>
 			<div className={classes.posts}>
@@ -20,16 +24,8 @@ const MyPosts = () => {
 				<div>
 					<button>Add Post</button>
 				</div>
-				
-				<Post src={postsData[0].url } name={postsData[0].name } message={postsData[0].message } likesCount={postsData[0].likeCount } />
-				<Post src={postsData[1].url } name={postsData[1].name } message={postsData[1].message } likesCount={postsData[1].likeCount } />
-				<Post src={postsData[2].url } name={postsData[2].name } message={postsData[2].message } likesCount={postsData[2].likeCount } />
-				<Post src={postsData[3].url } name={postsData[3].name } message={postsData[3].message } likesCount={postsData[3].likeCount } />
-				<Post src={postsData[4].url } name={postsData[4].name } message={postsData[4].message } likesCount={postsData[4].likeCount } />
-				<Post src={postsData[5].url } name={postsData[5].name } message={postsData[5].message } likesCount={postsData[5].likeCount } />
-				
-				
-			  </div>
+					{ postsElements }
+				</div>
 		  </div>
 			)
 	}
